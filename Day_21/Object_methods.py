293a7ebaf49b510a50f546a6935dbf159b7bb736
isildur2001 <palverarton@gmail.com>
2023-12-30 13:41:18 +0200

    Comments Off All

--- Day_21/Object_methods.py
+++ Day_21/Object_methods.py
@@ -1,44 +1,44 @@
-# class Person:
-#     def __init__ (self, firstname, lastname, age, country, city):
-#         self.firstname = firstname
-#         self.lastname = lastname
-#         self.age = age
-#         self.country = country
-#         self.city = city
+# # class Person:
+# #     def __init__ (self, firstname, lastname, age, country, city):
+# #         self.firstname = firstname
+# #         self.lastname = lastname
+# #         self.age = age
+# #         self.country = country
+# #         self.city = city
 
-# p = Person('Ginura', 'Amarasena', 25, 'Finland', 'Helsinki')
-# print(p.firstname)
-# print(p.lastname)
-# print(p.age)
-# print(p.country)
-# print(p.city)
+# # p = Person('Ginura', 'Amarasena', 25, 'Finland', 'Helsinki')
+# # print(p.firstname)
+# # print(p.lastname)
+# # print(p.age)
+# # print(p.country)
+# # print(p.city)
  
-'''p = Person() is creating a object in Person class by calling the class then the values are inserted to P object. 
-    Which will execute in init function using its parameters.  '''
+# '''p = Person() is creating a object in Person class by calling the class then the values are inserted to P object. 
+#     Which will execute in init function using its parameters.  '''
 
-# class Person:
-#     def __init__ (self, firstname, lastname, age, country, city):
-#         self.firstname = firstname
-#         self.lastname = lastname
-#         self.age = age
-#         self.country = country
-#         self.city = city
-#     def person_info(self):
-#         return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}'
+# # class Person:
+# #     def __init__ (self, firstname, lastname, age, country, city):
+# #         self.firstname = firstname
+# #         self.lastname = lastname
+# #         self.age = age
+# #         self.country = country
+# #         self.city = city
+# #     def person_info(self):
+# #         return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}'
 
-# p =Person('Asabeneh', 'Yetayeh', 25, 'Finland', 'Helsinki')
-# print(p.person_info())
+# # p =Person('Asabeneh', 'Yetayeh', 25, 'Finland', 'Helsinki')
+# # print(p.person_info())
 
-# # class Geek:
-#     #default constructor
-#     def __init__(self):
-#         self.geek = 'GeeksforGeeks'
-#     def print_Geek(self):
-#         print(self.geek)
+# # # class Geek:
+# #     #default constructor
+# #     def __init__(self):
+# #         self.geek = 'GeeksforGeeks'
+# #     def print_Geek(self):
+# #         print(self.geek)
 
-# obj1 = Geek()
+# # obj1 = Geek()
 
-# obj1.print_Geek()
+# # obj1.print_Geek()
 
 
 class Vehicle:
