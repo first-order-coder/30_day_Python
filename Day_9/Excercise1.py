2a605c494fe57c17fb827b0de1833fe07f1ecd53
Ginura-Amarasena <78877651+first-order-coder@users.noreply.github.com>
2024-01-01 20:12:51 +0200

    Delete Day_9 directory

--- Day_9/Excercise1.py
+++
@@ -1,7 +0,0 @@
-age = int(input('Enter Your Age:'))
-
-if age >= 18:
-    print('You are old enough to drive')
-elif age < 18:
-    need_to_wait = (18 - age)
-    print('You are not old enough to drive so you need to stay', need_to_wait ,'years to drive')
