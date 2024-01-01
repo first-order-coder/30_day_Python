72153ae0786e55ae351bf749d815445dc7a2f641
Ginura-Amarasena <78877651+first-order-coder@users.noreply.github.com>
2024-01-01 20:33:53 +0200

    Delete OpenCV directory

--- OpenCV/Code_camp/checking.py
+++
@@ -1,8 +0,0 @@
-import cPickle
-
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = cPickle.load(fo)
-    return dict
-
-unpickle(r'C:\Users\ginuram\Desktop\Dekstop\30DaysofPython\OpenCV\Code_camp\Assestes\cifar-10-batches-py\data_batch_1')
