f92b14a523f902df534cd40cd1d34e85717d9a85
first-order-coder <manethginura@gmail.com>
2024-01-02 13:55:11 +0200

    Reverting

---
+++ Day_9/Excercise2.1.py
@@ -0,0 +1,18 @@
+month = input('Enter The month:')
+
+Winter = ['December', 'January', 'February']
+Autmn = ['September', 'October', 'November']
+Spring = ['March', 'April', 'May']
+Summer = ['June', 'July', 'August']
+
+if month in Winter:
+    print('Its Winter Season')
+
+elif month in Autmn:
+    print('Its Autmn Season')
+
+elif month in Spring:
+    print('Its Spring Season')
+
+elif month in Summer:
+    print('Its Summer Season')
