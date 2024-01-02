9028250c09134f02f40d8609cf33ea930f4c3372
first-order-coder <manethginura@gmail.com>
2024-01-02 13:54:24 +0200

    Reverting

---
+++ Day_7/Excercise3.py
@@ -0,0 +1,18 @@
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+converted = set(age)
+
+length_age = len(age)
+length_converted = len(converted)
+
+if length_age > length_converted:
+    print('length age is Longer than Length of Convetered')
+else:
+    print('False')
+
+
+print(len(age))
+print(len(converted))
+
+print(len(age) > len(converted))
+
