40d1536d9a75d6a37ba10df00882a45f0009a8fb
first-order-coder <manethginura@gmail.com>
2025-12-18 02:25:28 +0300

    revision_2025

--- MINI_PROJECTS/to_do_CLI/all_in_one.py
+++ MINI_PROJECTS/to_do_CLI/all_in_one.py
@@ -12,8 +12,21 @@ if task_file.exists():
 else:
     tasks = []
 
-def add_task(tasks):
-    user_given_task = ' '.join(sys.argv[1::])
+"<--- Working with sys.argv--->"
+# def system_logic(tasks):
+#     if (sys.argv[1] == "add"):
+#         add_task(tasks)
+#     elif (sys.argv[1] == "load"):
+#         load_tasks(tasks)
+# system_logic(tasks)
+
+# def add_task(tasks):
+    # user_given_task = ' '.join(sys.argv[2::])
+    # tasks.append({"id":len(tasks)+1,"task":user_given_task, "status":False})
+    # with task_file.open("w") as f:
+    #     json.dump(tasks, f, indent=2)
+
+def add_task(user_given_task):
     tasks.append({"id":len(tasks)+1,"task":user_given_task, "status":False})
     with task_file.open("w") as f:
         json.dump(tasks, f, indent=2)
@@ -21,6 +34,17 @@ def add_task(tasks):
 def load_tasks(tasks):
     with task_file.open("r") as f:
         tasks = json.load(f)
-        return tasks
-        
-print(load_tasks(tasks)[0])
+        for task in tasks:
+            print(task)
+
+ops_lst = ["ADD", "LOAD"]
+def working_with_menu():
+    user_input = input("Enter operation:")
+    if (int(user_input) == 1):
+        add_task_input = input("Selected ADD Enter Your operation:")
+        add_task(add_task_input)
+
+    elif (int(user_input) == 2):
+        print("Selected LOAD and Your tasks are:\n")
+        load_tasks(tasks)
+working_with_menu()
