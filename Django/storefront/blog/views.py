6675918729d4bea50a86aaee16d52c1ea30f024c
firstordercoder <manethginura@gmail.com>
2026-01-12 22:41:24 +0300

    Day_03

--- Django/storefront/blog/views.py
+++ Django/storefront/blog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "title":"Blog Post 1",
+        "content": "Be not overcome with evil, but overcome evil with good",
+        "date_posted": "January 12, 2026"
+    },
+    {
+        "title":"Blog Post 2",
+        "content": "Pour your heart in to IT",
+        "date_posted": "January 12, 2026"
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        "posts":posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
--- Django/storefront/storefront/urls.py
+++ Django/storefront/storefront/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('admin/', admin.site.urls), 
+    path('', include('blog.urls')) #this is what the home page looks like
 ]
