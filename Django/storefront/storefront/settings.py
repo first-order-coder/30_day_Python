ab13e699c29da52a672ecfe6e9393c3cd46f2001
ginuraMA <manethginura@gmail.com>
2026-01-27 09:36:06 +0300

    Day_09

--- Django/storefront/storefront/settings.py
+++ Django/storefront/storefront/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
