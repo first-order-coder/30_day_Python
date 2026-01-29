f3e03438bf5a4f53f9905acbc2ec7263af0d7dc8
ginuraMA <manethginura@gmail.com>
2026-01-29 12:16:17 +0300

    Day_10

--- Django/storefront/storefront/urls.py
+++ Django/storefront/storefront/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings 
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('register/', user_views.register, name='register'),  
@@ -27,3 +31,8 @@ urlpatterns = [
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')) #this is what the home page looks like
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
