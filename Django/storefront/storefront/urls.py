0c0431f3c1f4cabab827e5f5a049df9818e366c9
ginuraMA <manethginura@gmail.com>
2026-02-02 21:17:18 +0300

    Day_11

--- Django/storefront/storefront/urls.py
+++ Django/storefront/storefront/urls.py
@@ -32,7 +32,5 @@ urlpatterns = [
     path('', include('blog.urls')) #this is what the home page looks like
 ]
 
-
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
