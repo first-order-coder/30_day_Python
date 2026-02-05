1238691c1597bd3ecf42a27cabc812d83c5f8d4b
ginuraMA <manethginura@gmail.com>
2026-02-05 12:31:19 +0300

    Day_12

--- Django/storefront/users/models.py
+++ Django/storefront/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -7,5 +8,16 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __repr__(self) -> str:
+    def __str__(self) -> str:
         return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300: #300 means pixels
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
