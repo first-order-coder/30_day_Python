efb5a57f2c233675242aebb8f17d9a5fab589b56
ginuraMA <manethginura@gmail.com>
2026-02-07 11:11:12 +0300

    Day_13

--- Django/storefront/blog/views.py
+++ Django/storefront/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,6 +9,15 @@ def home(request):
         "posts":Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #it will automatically look inside templates folder
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] #this order the posts from newest to oldest
+
+class PostDetailView(DetailView):
+    model = Post 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
