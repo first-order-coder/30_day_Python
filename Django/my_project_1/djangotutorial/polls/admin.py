348afad594ea04500ed0c9905626062ac8fe63e2
ginuraMA <manethginura@gmail.com>
2026-02-22 11:29:36 +0300

    Day_21

--- Django/my_project_1/djangotutorial/polls/admin.py
+++ Django/my_project_1/djangotutorial/polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QustionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        #fieldset(title_string, options_dictionary_inside_the_curly_braces)
+        ("Question Title", {"fields": ["question_text"]}), #first argument in the tuple is the name of the field
+        ("Date infomation", {"fields": ["pub_date"], "classes":["collapse"]}),] 
+        #fields = ["pub_date", "question_text"]#this makes the fileds come in order as they appear here
+    inlines = [ChoiceInLine]
+    
+admin.site.register(Question, QustionAdmin)
+
